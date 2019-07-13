c48cec778c2fbb30173387c8737948564896310f
1531157110 <52371867+1531157110@users.noreply.github.com>
2019-07-13 08:03:44 +0800

    性别部分数据集解析

---
+++ read-data/read.md
@@ -0,0 +1,9 @@
+这是读取数据集，并且筛选数据集的文件。
+
+```create_db.py```将数据集转为.mat格式
+
+```utils.py```是一个工具包文件，实现了图片的筛选等功能
+
+本文件读写IMDB-WIKI数据集，参考了
+
+```https://github.com/yu4u/age-gender-estimation```
---
+++ read-data/read_data.py
@@ -0,0 +1,20 @@
+﻿import numpy as np
+from utils import load_data
+from keras.utils import np_utils
+from pathlib import Path
+from matplotlib import pyplot as plt
+
+input_path = "outputdata128.mat"
+output_path = Path(__file__).resolve().parent.joinpath("a")
+output_path.mkdir(parents=True, exist_ok=True)
+image, gender, age, _, image_size, _ = load_data(input_path)
+X_data = image
+y_data_g = np_utils.to_categorical(gender, 2)	#one-hot
+y_data_a = np_utils.to_categorical(age, 101)	#one-hot
+
+#测试
+print("单张图片的像素值和通道数目{}".format(X_data[1].shape))
+#plt.imshow(X_data[2])
+#plt.show()
+print("图片总数以及性别分类个数{}".format(y_data_g.shape))
+print("图片总数以及年龄分类个数{}".format(y_data_a.shape))
